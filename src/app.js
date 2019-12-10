ca72a4cc8728b960cb162caf17fd7c57b8735bc4
Queen Shabazz <queennefertitishabazz@gmail.com>
2019-12-09 20:28:41 -0800

    database init

--- src/app.js
+++ src/app.js
@@ -1,1 +1,39 @@
-console.log("test")
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const { NODE_ENV } = require('./config')
+const usersRouter = require('./users/users-router')
+const recipesRouter = require('./recipes/recipes-router')
+
+const app = express()
+
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
+  skip: () => NODE_ENV === 'test',
+}))
+app.use(cors())
+app.use(helmet())
+
+app.use('/api/users', usersRouter)
+app.use('/api/recipes', recipesRouter)
+
+app.use(function errorHandler(error, req, res, next) {
+  let response
+  if (NODE_ENV === 'production') {
+    response = { error: 'Server error' }
+  } else {
+    console.error(error)
+    response = { error: error.message, object: error }
+  }
+  res.status(500).json(response)
+})
+
+app.get('users', (req, res)=>{
+    res.json()
+  })
+  
+  app.listen(8000, () =>{
+    console.log('Server started on PORT 8000');
+  })
+module.exports = app
