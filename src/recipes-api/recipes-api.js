380c3a8ba085c628850d13076f1d42df91dd377c
Queen Shabazz <queennefertitishabazz@gmail.com>
2020-01-29 17:51:41 -0800

    package cleaning

--- src/recipes-api/recipes-api.js
+++ src/recipes-api/recipes-api.js
@@ -1,7 +1,12 @@
+//TURN INTO FETCH REQUEST
+const request = require('request-promise');
+
 class RecipeApi {
     static getRecipe(ingredients, flavor, callback){
-       fetch(`http://www.recipepuppy.com/api/?i=${ingredients}%2C+&q=${flavor}/&minIngs=&maxIngs=&minRating=&minRates=20&onlyImages=1`
-       ).then((res)=>{
+       request({
+           uri: `http://www.recipepuppy.com/api/?i=${ingredients}%2C+&q=${flavor}/&minIngs=&maxIngs=&minRating=&minRates=20&onlyImages=1`,
+           json: true
+       }).then((res)=>{
            callback(res);
        }).catch((error)=>{
            //TODO figure out what to do when there's error
@@ -11,8 +16,10 @@ class RecipeApi {
 
 class RecipeApi2 {
     static get(ingredients, callback){
-       fetch(`http://www.recipepuppy.com/api/?i=${ingredients}%2C+&q=&minIngs=&maxIngs=&minRating=&minRates=20&onlyImages=1`)
-       .then((res)=>{
+       request({
+           uri: `http://www.recipepuppy.com/api/?i=${ingredients}%2C+&q=&minIngs=&maxIngs=&minRating=&minRates=20&onlyImages=1`,
+           json: true
+       }).then((res)=>{
            callback(res);
        }).catch((error)=>{
            //TODO figure out what to do when there's error
