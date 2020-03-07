2e47c5e0e9bd839dfb1e13452d6ff7a0821331b3
Queen <queennefertitishabazz@gmail.com>
2020-03-07 13:50:57 -0800

    userRecipesService

--- src/userrecipes/userrecipesroute.js
+++ src/userrecipes/userrecipesroute.js
@@ -1,6 +1,6 @@
 'use strict';
 const express = require('express');
-const useruserRecipesService = require('./userrecipesservice');
+const userRecipesService = require('./userrecipesservice');
 
 const {
   requireAuth
