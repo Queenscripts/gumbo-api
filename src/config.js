077437456b0b8d68b331403f4fc34d90320c9e08
Queen <queennefertitishabazz@gmail.com>
2020-03-07 14:01:05 -0800

    env

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://gumbo:gumbo@localhost/gumbo',
+    DB_URL: process.env.DB_URL || 'postgresql://gumbo@localhost/gumbo',
     TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://gumbo@localhost/gumbo-test',
     JWT_SECRET: process.env.JWT_SECRET || "change me",
     JWT_EXPIRY: "24h"
