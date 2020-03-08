7bef4c92acad9eb8c19b38918ecff7f7069c43ed
Queen <queennefertitishabazz@gmail.com>
2020-03-07 17:41:38 -0800

    userrecipes db creation

--- src/server.js
+++ src/server.js
@@ -2,6 +2,12 @@ const knex = require('knex')
 const app = require('./app')
 const { PORT, DB_URL } = require('./config')
 
+knex.schema.createTable('userrecipes', function (table) {
+  table.increments();
+  table.string('name');
+})
+Outputs:
+create table `userrecipes` (`id` int unsigned not null auto_increment primary key, `thumbnail` varchar(255), `title` varchar(255),  `ingredients` varchar(255), `recipeurl` varchar(255), 
 const db = knex({
   client: 'pg',
   connection: DB_URL,
